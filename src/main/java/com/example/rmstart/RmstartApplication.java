26317aeba874ad0fd059d5803325644c420ff3ed
zhouhongzhan <zhz666521@163.com>
2020-07-25 00:42:58 +0800

    rabbitMQ test first commit

---
+++ src/main/java/com/example/rmstart/RmstartApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmstartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rmstart/RmstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rmstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
